6b6f1f85bdcbe4f91736ef13cbe72441b3404ef2
Michael <mbg@outlook.com>
2018-06-05 15:50:33 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,11 @@ import Footer from '../components/HomeFooter'
 import cn from 'classnames'
 import Link from 'gatsby-link'
 
-class IndexPage extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      imageUrl: null
-    }
-  }
-
-  hoverImage = imageUrl => {
-    this.setState({ imageUrl })
-  }
-
-  endHover = () => {
-    this.setState({ imageUrl: false })
-  }
-
-  render () {
-    const { data } = this.props
-    const { creative } = data.site.siteMetadata
-
-    return (
-      <div>
-        {creative.map((c, index) => (
-          <div key={c.name} className='w-50-ns w-100 fl overflow-hidden'>
-            <div
-              className={cn(
+const IndexPage =({data: {site: {siteMetadata:{ creative }}}) => (
+<div>
+  {creative.map((c, index) => (
+    <div key={c.name} className='w-50-ns w-100 fl overflow-hidden'>
+    <div className={cn(
                 (index + 1) % 2 === 0 && 'mr0-ns',
                 (index + 1) % 2 !== 0 && 'mr4-ns',
                 'mb4'
@@ -47,11 +26,10 @@ class IndexPage extends Component {
             </div>
           </div>
         ))}
-        <Footer creative={data.site.siteMetadata.creative} />
+        <Footer creative={creative} />
       </div>
     )
-  }
-}
+
 
 export default IndexPage
 
