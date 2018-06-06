a51a1398faeab0adf4c82adc55c7e918d31c8b0f
Michael <mbg@outlook.com>
2018-06-06 07:25:42 +0100

    Update

--- src/components/Grow.js
+++ src/components/Grow.js
@@ -1,13 +1,12 @@
 import styled from 'styled-components'
 
 const Grow = styled.div.attrs({
-  className: 'secondary-front bg-white absolute top-0 right-0'
+  className: 'secondary-front bg-white fixed top-0 right-0'
 })`
 opacity: ${props => (props.show ? 1 : 0)}
 width: ${props => (props.show ? '100%' : 0)};
 height: ${props => (props.show ? '100%' : 0)};
-transition: all 500ms ease-out;
-
+transition: all 400ms ease-in;
 
 `
 export default Grow
