3b5019f9cd5bfe854a216b45039b054467af25c6
Michael <mbg@outlook.com>
2018-06-25 10:30:29 +0100

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,9 @@
-var contentful = require('./config/contentful.js')
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  require('dotenv').config({ path: './config/keys.env' })
+}
 
 module.exports = {
   pathPrefix: `/public`,
-  siteMetadata: {
-    creative: [
-      {
-        name: 'Better hearts, better cities',
-        link: '/better-hearts-better-cities',
-        image: 'MockupBusinessCard.png'
-      },
-      { name: 'Can you see me', link: '/can-you-see-me', image: '/test.jpg' },
-      {
-        name: 'What goes into making a medicine',
-        link: '/what-goes-into-making-a-medicine',
-        image: '/test.jpg'
-      },
-      { name: 'Renegade tea', link: '/renegade-tea', image: '/test.jpg' },
-      {
-        name: 'The Soft Power 30',
-        link: '/the-soft-power-30',
-        image: '/test.jpg'
-      },
-      {
-        name: "Claude's Cat Food",
-        link: '/claudes-cat-food',
-        image: '/test.jpg'
-      },
-      {
-        name: 'Childhood dementia',
-        link: '/childhood-dementia',
-        image: '/test.jpg'
-      },
-      { name: 'Early action', link: '/early-action', image: '/test.jpg' }
-    ]
-  },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
@@ -45,8 +15,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `${contentful.spaceId}`,
-        accessToken: `${contentful.accessToken}`
+        spaceId: `${process.env.SPACE_ID}`,
+        accessToken: `${process.env.ACCESS_TOKEN}`
       }
     },
     'gatsby-plugin-react-helmet',
