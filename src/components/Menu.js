cac3aa99979feb01eb979fc89bffa0ad8284925e
Michael <mbg@outlook.com>
2018-06-26 14:07:50 +0100

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Grow from './Grow'
 import Link from 'gatsby-link'
+import FaTimes from 'react-icons/lib/fa/times'
 
 const Menu = ({ showMenu, close }) => {
   return (
     <Grow show={showMenu}>
       <div onClick={close} className='pa3'>
-        <i className='pointer fr fa fa-times dark-gray mt2 fa-lg' />
+        <div className='pointer fr dark-gray mt2'>
+          <FaTimes />
+     </div>
       </div>
       <div className='f3 mt5 tc'>
         <div onClick={close}>
