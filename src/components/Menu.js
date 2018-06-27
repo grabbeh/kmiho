2586916faef5f701a997fa8642abdfadac1b4962
Michael <mbg@outlook.com>
2018-06-27 14:27:59 +0100

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import cn from 'classnames'
+import MdClose from 'react-icons/lib/md/close'
 
 const Menu = ({ showMenu, close }) => {
   return (
     <div className={cn(!showMenu && 'dn')}>
+       <div onClick={close} className='dn-ns'>
+          <div className='pointer dark-gray fr mt3 mr2'>
+              <MdClose size={24} />
+            </div>
+      </div>
       <div className='f3 mt5 tc'>
         <div onClick={close}>
           <Link
