7f7dabd8a73f77252bc5efa9ebec3259a9b9f886
Michael <mbg@outlook.com>
2018-08-02 17:49:35 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,24 +23,6 @@ module.exports = {
         }
       }
     },
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: './src/favicon.png',
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          twitter: false,
-          yandex: false,
-          windows: false
-        }
-      }
-    },
     `gatsby-plugin-netlify`
   ]
 }
